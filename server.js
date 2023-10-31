0ec3d907c23fab9de18ad54ebe3ce5cb8ed639f9
Renata Reinartz <112646473+renata1026@users.noreply.github.com>
2023-10-31 12:52:39 -0600

    add middleware

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
+import cors from 'cors';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { userRouter } from './routes/userRouter.js';
+dotenv.config();
+export const prisma = new PrismaClient();
+const app = express();
+app.use(cors());
+app.use(express.json());
 app.use(async (req, res, next) => {
   // check if there's an auth token in the header
   try {
@@ -26,4 +37,22 @@ app.use(async (req, res, next) => {
   } catch (error) {
     res.send({ success: false, error: error.message });
   }
+});
+
+app.use('/users', userRouter);
+
+//404 Route not found
+app.use((req, res, next) => {
+  res.status(404).send({ success: false, error: 'Route does not exist' });
+});
+
+//Error Handling Middleware
+app.use((error, req, res, next) => {
+  res.send({ success: false, error: error.message });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
